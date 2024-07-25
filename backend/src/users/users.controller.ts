b4cebe8cdead44bc56964be29960f4af3fcbdaed
Nanashii76 <pauloamino76@gmail.com>
2024-07-25 14:33:49 -0300

    Adicionado arquivos do projeto

---
+++ backend/src/users/users.controller.ts
@@ -0,0 +1,19 @@
+// users.controller.ts
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() user: Partial<User>): Promise<User> {
+    return this.usersService.create(user);
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+}
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,22 @@
+// users.service.ts
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(user: Partial<User>): Promise<User> {
+    const newUser = this.userRepository.create(user);
+    return this.userRepository.save(newUser);
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+}
